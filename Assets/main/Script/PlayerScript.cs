b750081b8ca96148856c4148adab0898465e751b
momoe13 <131223093+momoe13@users.noreply.github.com>
2024-12-18 00:15:45 +0900

    プレイヤープレハブ変更、アニメーション追加

--- Assets/main/Script/PlayerScript.cs
+++ Assets/main/Script/PlayerScript.cs
@@ -7,11 +7,11 @@ public class PlayerScript : MonoBehaviour
     [SerializeField]
     public float HP;      //HPの変数
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb;　
     private float horizontal;
     private float vertical;
 
-
+    private Animator animator;
 
     [SerializeField] private GameObject[] bullets;//武器を入れる配列
 
@@ -25,6 +25,9 @@ public class PlayerScript : MonoBehaviour
         //リジッドボディの取得
         rb = GetComponent<Rigidbody2D>();
 
+        //アニメーターの取得
+        animator = GetComponent<Animator>();
+
         //武器の要素数を取得
         weaponNumber = bullets.Length;
     }
@@ -32,9 +35,9 @@ public class PlayerScript : MonoBehaviour
     private void Update()
     {
         HandleInput();
+        MoveAnimation();
 
 
-        
         //Eキーを押したとき使用する武器を変える
         //武器番号を変更
         //現在の武器が武器番号の最後の時、武器番号をリセットする
@@ -89,5 +92,38 @@ public class PlayerScript : MonoBehaviour
     public float GetHp()
     {
         return HP;
+    }
+
+    private void MoveAnimation()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("Up", true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("Left", true);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("Down", true);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("Right", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A)
+            || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+
+        if (HP == 0)
+        {
+            animator.SetBool("Gameover", true);
+        }
     }
 }
